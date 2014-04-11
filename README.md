1eb8191634e28342d00cde794005b88ad15de91a
devowhippit <devonhirth@gmail.com>
2014-04-10 22:47:15 -0400

    extended options and plugin organization

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
 jquery.scrll
 ============
 
-Natural scroll-to plugin for mouse wheel and track pad scrolling.
+Auto scroll-to plugin for mouse wheel and track pad scrolling. Jumps from section to section of webpage using CSS hardware accelerated transitions. Includes options to jump to the top of the page to the bottom and vise versa. 
+
+Default Options
+===============
+
+<table>
+<tr>
+	<td>$scrollparent   </td><td> $( '#wrap' )</td>
+</tr>
+<tr>
+	<td>$scrollchildren </td><td> $( '#wrap' ).find( '.container' )</td>
+</tr>
+<tr>
+	<td>$navtranslate   </td><td> $( '.nav-translate' ).find( 'li.translate a' )</td>
+</tr>
+<tr>
+	<td>delay           </td><td> 1000</td>
+</tr>
+<tr>
+	<td>scrolling       </td><td> false</td>
+</tr>
+<tr>
+	<td>snapToTop       </td><td> false</td>
+</tr>
+<tr>
+	<td>snapToBottom    </td><td> false</td>
+</tr>
+<tr>
+	<td>sectionClass    </td><td> 'container'</td>
+</tr>
+<tr>
+	<td>activeClass     </td><td> 'inview'</td>
+</tr>
+<tr>
+	<td>initIndexFocus  </td><td> 1</td>
+</tr>
+<tr>
+	<td>initFocus       </td><td> ''</td>
+</tr>
+<tr>
+	<td>jqueryAnimSpeed </td><td> 450</td>
+</table>
--- jquery.scrll.js
+++ jquery.scrll.js
@@ -1,80 +1,208 @@
 
 ( function( $ ) {
 
-    var $scrollparent   = $( '#wrap' );
-    var $scrollchildren = $scrollparent.find( '.container' );
+    if ( typeof Modernizr !== 'undefined' && Modernizr.touch || !Modernizr.csstransforms ) {
+        return;
+    }
+
+    var $document       = $( document );
+    var $window         = $( window );
+    var $body           = $( 'body' );
     var inviewcount     = 0;
     var timers          = [];
-    var delay           = 1000;
-    var scrolling       = false;
+    var scrll           = false;
 
-    $scrollchildren.each( function() {
-        if ( $( this ).hasClass( 'inview' ) ) {
+    var options = {
+        $scrollparent   : $( '#wrap' ),
+        $scrollchildren : $( '#wrap' ).find( '.container' ),
+        $navtranslate   : $( '.nav-translate' ).find( 'li.translate a' ),
+        delay           : 1000,
+        scrolling       : false,
+        snapToTop       : false,
+        snapToBottom    : false,
+        sectionClass    : 'container',
+        activeClass     : 'inview',
+        initIndexFocus  : 1,
+        initFocus       : '',
+        jqueryAnimSpeed : 450
+    };
+
+    options.$scrollchildren.each( function() {
+        if ( $( this ).hasClass( options.activeClass ) ) {
             inviewcount = inviewcount++;
+            options.initIndexFocus = $( this ).index();
         }
     });
 
-    if ( inviewcount === 0 ) {
-        $scrollchildren.first().addClass( 'inview' );
-    }
+    options.initFocus = options.$scrollchildren.eq( options.initIndexFocus );
 
     /**
-     * Transistions the page to the target div offset top
+     * Translates the page to the target div offset top.
+     * If you don't pass it a target it will use the object
+     * the function is bound to. Any selector can be used for
+     * the target as long as it is unique.
      * @param  {string} target optional selector for target.
      * @return {null}
      */
     $.fn.focustranslate = function( target ) {
-        console.log( target );
-        var scrolltop = typeof target === 'undefined' ? -( $( this ).position().top ) : -( $( target ).position().top );
-        var translate = 'translate3d( 0 , ' + scrolltop + 'px , 0 )';
-        $scrollparent.css( '-webkit-transform' , translate ).css( 'transform' , translate );
+        var scrolltop, translate;
+        var $target = typeof target === 'undefined' ? $( this ) : $( target );
+
+        /**
+         * Prevents exposing the bottom of the page by making sure the height
+         * of the next target and following targets are greater than the window
+         * height, otherwise it will scroll to the top of the target.
+         */
+        if ( $target.height() + $target.nextAll().height() < $window.height() ) {
+            scrolltop = -( $target.position().top + $target.height() - $window.height() );
+        } else {
+            scrolltop = -( $target.position().top );
+        }
+
+        /**
+         * If Scrolling is bound utilize css smooth scroll,
+         * else resort to using jquery animate.
+         * @type {[type]}
+         */
+        if ( scrll === true ) {
+            translate = 'translate3d( 0 , ' + scrolltop + 'px , 0 )';
+            options.$scrollparent.css( '-webkit-transform' , translate ).css( 'transform' , translate );
+        } else {
+            $( 'html , body' ).animate( { scrollTop: Math.abs( scrolltop ) + 'px' } , options.jqueryAnimSpeed );
+        }
+
+        $( '.scrll-nav' ).find( 'li' ).eq( $target.index() ).addClass( options.activeClass );
     };
 
-    $( document ).ready( function() {
-
-        $( '.nav-translate' ).find( 'li.translate a' ).bind( 'click' , function( event ) {
-
-            event.preventDefault();
-
-            var target = $( this ).attr( 'rel' );
-
-            $( '.inview' ).removeClass( 'inview' );
-            $( '.container.' + target ).addClass( 'inview' );
-            $( '.inview' ).focustranslate();
-
-        } );
-
-    });
-
-    $( document ).ready().bind( 'mousewheel DOMMouseScroll' , function( event ) {
-      
-        var delta, next, prev, focal_index;
-
-        delta = event.originalEvent.wheelDelta || -event.originalEvent.detail;
+    /**
+     * Click Event. 
+     * Looks for the next target in the rel attribute of click target.
+     * Looks for back-to-top rel for jumping to the top of the page.
+     * Looks for natural scrolling link for disabling plugin.
+     * @return {[type]} [description]
+     */
+    function clickEvent( event ) {
 
         event.preventDefault();
 
-        if ( scrolling ) { return; }
+        var target = $( this ).attr( 'rel' );
 
-        focal_index = $scrollparent.find( '.container.inview' ).index();
-
-        $( '.inview' ).removeClass( 'inview' );
-
-        if ( delta < 0 ) {
-            next = $scrollchildren.eq( focal_index ).nextAll( '.container:first' );
-            next = ( next.length > 0 ) ? next : $scrollchildren.eq( 0 );
-            next.addClass( 'inview' );
-        } else if ( delta > -0 ) {
-            prev = $scrollchildren.eq( focal_index ).prevAll( '.container:first' );
-            prev = ( prev.length > 0 ) ? prev : $scrollchildren.eq( $scrollchildren.length - 1 );
-            prev.addClass( 'inview' );
+        if ( target === 'natural-scrolling' ) {
+            $document.unbindScrll(); return;
+        } else if ( target === 'css-scrolling' ) {
+            $document.bindScrll(); return;
         }
 
-        scrolling = true;
-        timers[0] = setTimeout( function() { scrolling = false; } , delay );
+        $( '.'+options.activeClass ).removeClass( options.activeClass );
 
-        $( '.inview' ).focustranslate();
+        if ( target === 'back-to-top' ) {
+            options.$scrollchildren.eq( 0 ).addClass( options.activeClass );
+        } else {
+            $( '.'+options.sectionClass+'.'+target ).addClass( options.activeClass );
+        }
 
-    });
+        $( '.'+options.activeClass ).focustranslate();
+    }
+
+    /**
+     * Scroll Event
+     * @param  {[type]} event [description]
+     * @return {[type]}       [description]
+     */
+    function scrllEvent( event ) {
+
+        event.preventDefault();
+        var delta, focal_index, focal_difference = 0, $target;
+
+        /**
+         * Get the scroll change.
+         */
+        delta = event.originalEvent.wheelDelta || -event.originalEvent.detail;
+
+        /**
+         * Do not scroll if already scrolling.
+         */
+        if ( options.scrolling ) { return; }
+
+        /**
+         * Selects next target.
+         * Selects target at the top if at the bottom.
+         * Selects target at the bottom if at the top.
+         */
+        focal_index = options.$scrollparent.find( '.'+options.sectionClass+'.'+options.activeClass ).index() + focal_difference;
+
+        if ( delta < 0 ) {
+            $target = options.$scrollchildren.eq( focal_index ).nextAll( '.'+options.sectionClass+':first' );
+            $target = ( $target.length === 0 && options.snapToTop ) ? options.$scrollchildren.eq( 0 ) : $target;
+        } else if ( delta > -0 ) {
+            $target = options.$scrollchildren.eq( focal_index ).prevAll( '.'+options.sectionClass+':first' );
+            $target = ( $target.length === 0 && options.snapToBottom ) ? options.$scrollchildren.eq( options.$scrollchildren.length - 1 ) : $target;
+        }
+
+        /**
+         * Don't Scroll if there is no target.
+         */
+        if ( $target.length === 0 ) { return; }
+        $( '.' + options.activeClass ).removeClass( options.activeClass );
+        $target.addClass( options.activeClass );
+
+        /**
+         * Reset scrolling var.
+         */
+        options.scrolling = true;
+        timers[ 0 ] = setTimeout( function() { options.scrolling = false; } , options.delay );
+
+        /**
+         * Translate to next target.
+         */
+        $( '.' + options.activeClass ).focustranslate();
+
+    }
+
+    /**
+     * [bindScrll description]
+     * @return {[type]} [description]
+     */
+    $.fn.bindScrll = function() {
+        $( this ).bind( 'mousewheel DOMMouseScroll' , scrllEvent );
+        scrll = true; $body.addClass( 'cssScroll' );
+
+        $( 'html , body' ).animate( { scrollTop: '0px' } , options.jqueryAnimSpeed );
+        options.$scrollchildren.eq( options.initIndexFocus ).addClass( options.activeClass );
+        $( '.'+options.activeClass ).focustranslate();
+    };
+
+    /**
+     * [unbindScrll description]
+     * @param  {[type]} event [description]
+     * @return {[type]}       [description]
+     */
+    $.fn.unbindScrll = function() {
+        $( this ).unbind( 'mousewheel DOMMouseScroll' , scrllEvent );
+        scrll = false; $body.removeClass( 'cssScroll' );
+        
+        $( 'html , body' ).animate( { scrollTop: Math.abs( options.initFocus.position().top ) + 'px' } , options.jqueryAnimSpeed );
+        options.$scrollparent.removeAttr( 'style' );
+    };
+
+    /**
+     * Bind Click Events
+     * @return {[type]} [description]
+     */
+    $.fn.bindClicks = function() {
+        $( this ).bind( 'click' , clickEvent );
+    };
+
+    /**
+     * When the Document is loaded
+     * @return {[type]} [description]
+     */
+    $document.ready( function() {
+
+        options.$navtranslate.bindClicks();
+        $( '.scrll' ).bindClicks();
+        $( '.scrll-nav' ).find( 'a' ).bindClicks();
+
+    }).bindScrll();
 
 })( jQuery );
